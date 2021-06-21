5ad90145b00d92fd3ca5bb5d46c9f808e478ab27
Amin Asadi <aminasadiam@outlook.com>
2021-06-21 14:56:54 +0430

    Add User Model

---
+++ src/Models/User/User.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Manawork.Models.User
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}

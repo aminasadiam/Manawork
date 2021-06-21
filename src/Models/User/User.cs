537e91a3bd408c0a389dca61d5e8df40eae017c6
Amin Asadi <aminasadiam@outlook.com>
2021-06-21 21:17:19 +0430

    Change Project File and add new Migration

--- src/Models/User/User.cs
+++ src/Models/User/User.cs
@@ -1,13 +1,28 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Manawork.Models.User
 {
     public class User
     {
+        [Key]
         public int UserId { get; set; }
+
+        [Display(Name = "Username")]
+        [MaxLength(250)]
+        [Required]
         public string Username { get; set; }
+
+        [Display(Name = "Email")]
+        [MaxLength(250)]
+        [Required]
         public string Email { get; set; }
+
+        [Display(Name = "Password")]
+        [MaxLength(250)]
+        [Required]
         public string Password { get; set; }
+        
         public DateTime RegisterDate { get; set; }
         public bool IsDelete { get; set; }
     }
